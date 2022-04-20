3d27d1d80a7a7a73cac769add7e8e1e28d1883eb
makck <joel.mak32@gmail.com>
2022-04-20 18:12:53 +0800

    Update config file for db

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   development: {
-    username: '<YOUR_USER_NAME>',
+    username: 'Chan Keet',
     password: null,
-    database: '<YOUR_APP_NAME>_development',
+    database: 'read_later_development',
     host: '127.0.0.1',
     dialect: 'postgres',
   },
