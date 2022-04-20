ec16e6b8c36c9eb6924580e60e1d51d9d4d24c14
makck <joel.mak32@gmail.com>
2022-04-20 23:00:06 +0800

    Set up base layout of app in App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,22 @@ export default function App() {
     <div className="container">
       <div className="row">
         <div className="col">
-          <form>
-            <input type="text" placeholder="Link to Article" />
-            <button type="button">Add link to list</button>
+          <form className="form-inline">
+            <input className="form-control mb-2 mr-sm-2" type="text" placeholder="Link to Article" />
+            <button className="btn btn-secondary mb-2" type="button">Add to list</button>
           </form>
+        </div>
+      </div>
+
+      <div className="row mt-4">
+        <div className="col">
+          <h2>Saved Articles</h2>
+          <ul className="list-group">
+            <li className="list-group-item">
+              <input className="form-check-input me-1" type="checkbox" value="" aria-label="..." />
+              First checkbox
+            </li>
+          </ul>
         </div>
       </div>
     </div>
