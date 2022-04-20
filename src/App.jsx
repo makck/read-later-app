61431fdb872c748191c1b5f92382feca0c370481
makck <joel.mak32@gmail.com>
2022-04-20 22:30:49 +0800

    Fix webpack and webpack html plugin config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 export default function App() {
-  return <div>This is App.jsx</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <form>
+            <input type="text" placeholder="Link to Article" />
+            <button type="button">Add link to list</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
