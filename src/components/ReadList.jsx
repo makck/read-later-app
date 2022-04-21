8c2b19a164a18871f47997e5356e344fdc64796a
makck <joel.mak32@gmail.com>
2022-04-21 16:38:56 +0800

    Change key of ReadList for unique key

--- src/components/ReadList.jsx
+++ src/components/ReadList.jsx
@@ -8,7 +8,7 @@ export default function ReadList({ completedList }) {
         <h2>Read List</h2>
         <ul className="list-group">
           {completedList.map((link) => (
-            <li key={link} className="list-group-item d-flex justify-content-between">
+            <li key={`read-${link}`} className="list-group-item d-flex justify-content-between">
               {link}
               <input className="form-check-input me-1 pull-right" type="checkbox" />
             </li>
